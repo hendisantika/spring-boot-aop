710d8e86611631831173fd234946014093add2d9
Hendi Santika <hendisantika@gmail.com>
2018-02-15 06:42:28 +0700

    #3 - Add Student Domain

---
+++ src/main/java/com/hendisantika/springbootaop/domain/Student.java
@@ -0,0 +1,71 @@
+package com.hendisantika.springbootaop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-aop
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/02/18
+ * Time: 06.21
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String passportNumber;
+
+    public Student() {
+        super();
+    }
+
+    public Student(Long id, String name, String passportNumber) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.passportNumber = passportNumber;
+    }
+
+    public Student(String name, String passportNumber) {
+        super();
+        this.name = name;
+        this.passportNumber = passportNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Student [id=%s, name=%s, passportNumber=%s]", id, name, passportNumber);
+    }
+
+}
