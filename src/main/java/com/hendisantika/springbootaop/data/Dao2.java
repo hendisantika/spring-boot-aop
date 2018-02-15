bf3459650a7b8d73f20df1ef5a4189da65cd131f
Hendi Santika <hendisantika@gmail.com>
2018-02-16 06:37:45 +0700

    #6 - Add Dao Service 2

---
+++ src/main/java/com/hendisantika/springbootaop/data/Dao2.java
@@ -0,0 +1,22 @@
+package com.hendisantika.springbootaop.data;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-aop
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/02/18
+ * Time: 06.37
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public class Dao2 {
+
+    public String retrieveSomething() {
+        return "Dao2";
+    }
+
+}
